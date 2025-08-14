13e10751da5e3be1e769e66287389263cc6de375
Norbert Micheel <nm@lautundschoen.de>
2025-08-14 17:37:01 +0200

    werbung entfernt

--- pages/yform.rapidmail.settings.php
+++ pages/yform.rapidmail.settings.php
@@ -70,15 +70,5 @@ $listFragment->setVar('content', $table, false);
 		<?= $listFragment->parse('core/page/section.php') ?>
 	</div>
 	<div class="col-lg-4">
-	<?php
-
-$anchor = '<a target="_blank" href="https://donate.alexplus.de/?addon=yform_rapidmail"><img src="'.rex_url::addonAssets('events', 'jetzt-beauftragen.svg').'" style="width: 100% max-width: 400px;"></a>';
-
-$fragment = new rex_fragment();
-$fragment->setVar('class', 'info', false);
-$fragment->setVar('title', $this->i18n('yform_rapidmail_donate'), false);
-$fragment->setVar('body', '<p>' . $this->i18n('yform_rapidmail_info_donate') . '</p>' . $anchor, false);
-echo !rex_config::get("alexplusde", "donated") ? $fragment->parse('core/page/section.php') : "";
-?>
 	</div>
 </div>
